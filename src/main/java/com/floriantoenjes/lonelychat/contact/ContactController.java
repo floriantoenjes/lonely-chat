d27e561324d2d556894843eb9abfe43034ced50d
Florian Tönjes <skatepainter@yahoo.de>
2019-07-28 12:11:15 +0200

    Created ContactController

---
+++ src/main/java/com/floriantoenjes/lonelychat/contact/ContactController.java
@@ -0,0 +1,40 @@
+package com.floriantoenjes.lonelychat.contact;
+
+import com.floriantoenjes.lonelychat.user.User;
+import com.floriantoenjes.lonelychat.user.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static com.floriantoenjes.lonelychat.utils.AuthUtils.getUsernameFromAuth;
+
+@Controller
+@RequestMapping("/contacts")
+public class ContactController {
+
+    private ContactRepository contactRepository;
+
+    private UserRepository userRepository;
+
+    public ContactController(ContactRepository contactRepository, UserRepository userRepository) {
+        this.contactRepository = contactRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping()
+    public Flux<Contact> getContacts() {
+        Mono<String> owner = getUsernameFromAuth();
+        return findOrCreateUser(owner).flatMapMany(contact -> contactRepository.findAllByOwnerId(contact.getId()));
+    }
+
+    // TODO: Extract from here and MessageController to keep code dry
+    private Mono<User> findOrCreateUser(Mono<String> username) {
+        return username.flatMap(name -> userRepository.findByUsername(name)).switchIfEmpty(createUser(username));
+    }
+
+    private Mono<User> createUser(Mono<String> username) {
+        return username.flatMap(name -> userRepository.save(new User(name)));
+    }
+}
--- src/main/java/com/floriantoenjes/lonelychat/contact/ContactRepository.java
+++ src/main/java/com/floriantoenjes/lonelychat/contact/ContactRepository.java
@@ -1,10 +1,13 @@
 package com.floriantoenjes.lonelychat.contact;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ContactRepository extends ReactiveMongoRepository<Contact, String> {
 
     Mono<Contact> findByOwnerIdAndTargetId(String ownerId, String targetId);
+
+    Flux<Contact> findAllByOwnerId(String ownerId);
 
 }
